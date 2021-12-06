7409ae8aeb483bc65ad17c2980ed643a4c96016d
amit bhura <56560994+bhurz@users.noreply.github.com>
2021-12-05 22:09:50 -0800

    Day 6 solution

---
+++ day6.py
@@ -0,0 +1,43 @@
+import collections
+import re
+
+MAPSIZE = 1000
+
+def ParseFile( filePath ):
+    print( '* Loading\t%s' % filePath )
+    age = []
+    with open( filePath, 'r', newline='' ) as fileHandle:
+        lines = fileHandle.readlines()
+        for line in lines:
+            age = [ int(val )for val in line.split(",") ]
+    
+    return age
+
+def LanternFishCountOptimized( input, days ):
+    ageCounter = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0 }
+    initialAgeCounter = collections.Counter(input)
+    for k,v in initialAgeCounter.items():
+        ageCounter[k] = v
+
+    for _ in range(days):
+        prev = 0
+        curr = 0
+        for i in range(8, -1, -1 ):
+            curr = ageCounter[i]
+            ageCounter[i] = prev
+            prev = curr
+
+        if curr > 0:
+            ageCounter[6] += curr
+            ageCounter[8] += curr
+
+    return sum(ageCounter.values())
+
+if __name__ == '__main__':
+    ageList = ParseFile( "D:\\Github\\aoc_2021\\day6_sample.csv" )
+   
+    ans = LanternFishCountOptimized(ageList, 80 )
+    print("Output 1:{}".format(ans))
+
+    ans = LanternFishCountOptimized(ageList, 256 )
+    print("Output 2:{}".format(ans))

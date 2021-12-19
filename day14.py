1e76633260e4a01dbf597294910307eb1a597773
amit <abhura@treyarch.com>
2021-12-19 14:12:29 -0800

    Day 14 optimised solution

--- day14.py
+++ day14.py
@@ -2,39 +2,50 @@ import collections
 import re
 import sys
 from collections import deque
+import heapq
 
 class Solution:
-    def __init__( self, filePath ):
-        self.graph = []
-
+    def __init__( self, filePath, nTiles ):
+        g = []
         with open( filePath, 'r' ) as fileHandle:
             lines = fileHandle.readlines()
             for line in lines:
-                self.graph.append( list( map( int, list(line.strip())) ) )
+                g.append( list( map( int, list(line.strip())) ) )
 
-    def FindMinumumWeightNode(self):
-        minWeight = sys.maxsize
-        minu = 0
-        minv = 0
-        for key in self.D:
-            u , v = key
-            val = self.D[(u,v)]
-            if val < minWeight and (u,v) not in self.dst:
-                minWeight = val
-                minu = u
-                minv = v
-        return minWeight, minu, minv
+        vertices = len(g)
+
+        self.graph = [[0]*vertices*nTiles for _ in range(vertices*nTiles) ]
+        for row in range(vertices*nTiles):
+            row_increment = row//vertices
+            base_row = row % vertices
+            for col in range(vertices*nTiles):
+                col_increment = col//vertices
+                base_col = col%vertices
+                val = g[base_row][base_col] + row_increment + col_increment
+                
+                if val >= 10:
+                    val -= 9
+
+                self.graph[row][col] = val
 
     def SolvePart1( self ):
         vertices = len( self.graph )
-        self.dst = set()
+        self.heap = [(0,0,0)]
         self.D = {}
         self.D[ ( 0, 0 ) ] = 0
-        
+        self.visited = set()
+
         directions = [(0,1), (0, -1), (1,0), (-1, 0)]
-        while len(self.dst) < vertices*vertices:
-            w, u, v = self.FindMinumumWeightNode()
-            self.dst.add( (u, v ) )
+        while len(self.visited) < vertices*vertices:
+            w, u, v = heapq.heappop( self.heap )
+            if (u, v ) == ( vertices -1, vertices-1 ):
+                break
+
+            if ( u, v ) not in self.visited:
+                self.visited.add( (u, v ) )
+            else:
+                continue
+
             for direction in directions:
                 new_u = u + direction[0]
                 new_v = v + direction[1]
@@ -42,12 +53,14 @@ class Solution:
                     new_w = w + self.graph[new_u][new_v]
                     if (new_u, new_v) not in self.D:
                         self.D[( new_u, new_v )] = new_w
+                        heapq.heappush( self.heap, (new_w, new_u, new_v ) )
                     elif (new_u, new_v) in self.D and new_w < self.D[( new_u, new_v )]:
                         self.D[( new_u, new_v )] = new_w
+                        heapq.heappush( self.heap, (new_w, new_u, new_v ) )
 
         return self.D[( vertices -1, vertices-1 )]
 
 if __name__ == '__main__':
-    obj = Solution( "D:\\Github\\aoc_2021\\day14_input.txt" )
+    obj = Solution( "D:\\Github\\aoc_2021\\day14_input.txt", 5 )
     ans = obj.SolvePart1()
     print(ans)
